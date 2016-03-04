e1899010b7a8a87c31753072b6ee7e2412a79599
Maxim Grishkov <mocyk@meta.ua>
2016-03-05 00:27:12 +0200

    single item view

--- app/modules/items/js/controllers.js
+++ app/modules/items/js/controllers.js
@@ -1,5 +1,11 @@
 'use strict'
 angular.module('app.items.controllers', [])
-.controller('allItemsController', ['$scope', 'Item', function($scope, Item) {
-  $scope.allItems = Item.query();
+.controller('allItemsController', ['$scope', 'Api', function($scope, Api) {
+  $scope.allItems = Api.Item.query();
+}])
+.controller('singleItemController', ['$scope', '$stateParams', 'Api', function($scope, $stateParams, Api) {
+  $scope.closeItem = function() {
+    $state.go('allItems');
+  }
+  $scope.item = Api.Item.get({ id:$stateParams.id });
 }]);
