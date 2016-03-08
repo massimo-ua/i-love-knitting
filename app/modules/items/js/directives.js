17fe6ba02c4e5ccd56c35fc5b9a46c38904135ea
Maxim Grishkov <mocyk@meta.ua>
2016-03-08 19:38:36 +0200

    comments form

--- app/modules/items/js/directives.js
+++ app/modules/items/js/directives.js
@@ -1,2 +1,23 @@
 'use strict'
-angular.module('app.items.directives', []);
+angular.module('app.items.directives', [])
+.directive('itemComments', [function(){
+  return {
+    restrict: 'AEC',
+    scope: {
+      itemInstance: '='
+    },
+    replace: true,
+    link: function(scope, elem, attrs) {
+      scope.saveComment = function() {
+        var savedItemInstance = angular.copy(scope.itemInstance);
+        scope.comment.datePublished = new Date();
+        savedItemInstance.comments.unshift(scope.comment);
+        scope.itemInstance.comments.unshift(scope.comment);
+        scope.comment = {};
+        scope.commentForm.$setPristine();
+        savedItemInstance.$update();
+      }
+    },
+    templateUrl: 'modules/items/views/comments.html'
+  }
+}]);
